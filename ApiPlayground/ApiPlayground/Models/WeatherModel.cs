65c0d199ed4ada1121f832042d4f58c626065791
Thomas <thomas@dillingdata.se>
2021-11-28 21:39:51 +0100

    Added the views

---
+++ ApiPlayground/ApiPlayground/Models/WeatherModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiPlayground.Models
+{
+    public class WeatherModel
+    {
+        public int Temperature { get; set; }
+
+        public int WindSpeed { get; set; }
+
+        public bool IsItRaining { get; set; }
+
+        public int RainQuantity { get; set; }
+
+    }
+}
