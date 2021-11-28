cf8beafe549df7e94792af75eea915bedb06f867
Thomas <thomas@dillingdata.se>
2021-11-28 21:20:37 +0100

    Added correct controllers

---
+++ ApiPlayground/ApiPlayground/Controllers/WeatherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPlayground.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
