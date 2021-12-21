aaddb090b09b42f135807ecdf478dd44c228bdda
Thomas <thomas@dillingdata.se>
2021-12-21 22:58:31 +0100

    working in dev version but not when published

--- ApiPlayground/ApiPlayground/Controllers/WeatherController.cs
+++ ApiPlayground/ApiPlayground/Controllers/WeatherController.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using System.Net;
 using System.Text;
 using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace ApiPlayground.Controllers
 {
@@ -18,29 +19,27 @@ namespace ApiPlayground.Controllers
             var lat = HttpContext.Session.GetString("LAT");
             var longt = HttpContext.Session.GetString("LONGT");
 
-            if(lat == null)
-            {
-                //something
-            }
-            else
-            {
-                string apiKey = "bef904c9d4916fca8184a376a9534a49";
+            string apiKey = "bef904c9d4916fca8184a376a9534a49";
 
-                //Request for current weather API
-                //var requestURI = string.Format("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longt + "&units=metric&appid=" + apiKey);
+            Debug.WriteLine(lat);
+            Debug.WriteLine(longt);
+            Debug.WriteLine(apiKey);
 
-                //Request for OneCallAPI
-                var requestURI = string.Format("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + longt + "&exclude={part}&appid=" + apiKey);
-                var request = WebRequest.Create(requestURI);
-                var response = request.GetResponse();
+            //Request for current weather API
+            //var requestURI = string.Format("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longt + "&units=metric&appid=" + apiKey);
 
-                Stream stream = response.GetResponseStream();
-                Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
-                StreamReader readstream = new StreamReader(stream, encode);
-                string s = readstream.ReadToEnd();
+            //Request for OneCallAPI
+            var requestURI = string.Format("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + longt + "&units=metric&appid=" + apiKey);
+            var request = WebRequest.Create(requestURI);
+            var response = request.GetResponse();
 
-                root = JsonConvert.DeserializeObject<Root>(s);
-            }
+            Stream stream = response.GetResponseStream();
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+            StreamReader readstream = new StreamReader(stream, encode);
+            string s = readstream.ReadToEnd();
+
+            root = JsonConvert.DeserializeObject<Root>(s);
+            
             return View(root);
         }
 
@@ -48,6 +47,7 @@ namespace ApiPlayground.Controllers
         {
             HttpContext.Session.SetString("LAT", lat);
             HttpContext.Session.SetString("LONGT", longt);
+            Debug.WriteLine("Data saved in session");
         }
     }
 }
