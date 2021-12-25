a6365ad63de51b8d72fed6f5b366aff29c6a4176
Thomas <thomas@dillingdata.se>
2021-12-26 00:02:06 +0100

    added state to weather cards

--- ApiPlayground/ApiPlayground/wwwroot/js/Card.js
+++ ApiPlayground/ApiPlayground/wwwroot/js/Card.js
@@ -1,31 +1,77 @@
 ﻿var code = document.getElementById("code").innerHTML;
 var PictureLink = document.getElementById("weatherPic");
 
+var card1state = 0;
+var card2state = 0;
+var card3state = 0;
+var card4state = 0;
+var card5state = 0;
+var card6state = 0;
+var card7state = 0;
+
 window.onload = function changeRef() {
     PictureLink.setAttribute('src', "https://openweathermap.org/img/wn/" + code + ".png");
-    
-};
-
-function sendWeekday() {
-
-
-    var request = new XMLHttpRequest;
-
-    //for published version
-    request.open("GET", '/ApiPlayground/Weather/StoreCoord?lat=' + latShort + '&longt=' + longShort);
-
-    //For developer version
-    //request.open("GET", 'Weather/StoreCoord?lat=' + latShort + '&longt=' + longShort);
-
-
-    request.onreadystatechange = function () {
-        // Check if the request is compete and was successful
-        if (this.readyState === 4 && this.status === 200) {
-            // Inserting the response from server into an HTML element
-            //x.innerHTML = this.responseText;
-        } else {
-            //x.innerHTML = "THIS DIDNT WORK"
-        }
-    };
-    request.send();
+}
+function onclickday1() {
+    if (card1state == 0) {
+        document.getElementById("body1").style.display = "block";
+        card1state = 1;
+    } else {
+        document.getElementById("body1").style.display = "none";
+        card1state = 0;
+    }
+}
+function onclickday2() {
+    if (card2state == 0) {
+        document.getElementById("body2").style.display = "block";
+        card2state = 1;
+    } else {
+        document.getElementById("body2").style.display = "none";
+        card2state = 0;
+    }
+}
+function onclickday3() {
+    if (card3state == 0) {
+        document.getElementById("body3").style.display = "block";
+        card3state = 1;
+    } else {
+        document.getElementById("body3").style.display = "none";
+        card3state = 0;
+    }
+}
+function onclickday4() {
+    if (card4state == 0) {
+        document.getElementById("body4").style.display = "block";
+        card4state = 1;
+    } else {
+        document.getElementById("body4").style.display = "none";
+        card4state = 0;
+    }
+}
+function onclickday5() {
+    if (card5state == 0) {
+        document.getElementById("body5").style.display = "block";
+        card5state = 1;
+    } else {
+        document.getElementById("body5").style.display = "none";
+        card5state = 0;
+    }
+}
+function onclickday6() {
+    if (card6state == 0) {
+        document.getElementById("body6").style.display = "block";
+        card6state = 1;
+    } else {
+        document.getElementById("body6").style.display = "none";
+        card6state = 0;
+    }
+}
+function onclickday7() {
+    if (card7state == 0) {
+        document.getElementById("body7").style.display = "block";
+        card7state = 1;
+    } else {
+        document.getElementById("body7").style.display = "none";
+        card7state = 0;
+    }
 }
