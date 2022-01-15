a82dc3b667ac85007816f0a62c014e169ccf9fe9
Thomas <thomas@dillingdata.se>
2022-01-15 12:26:52 +0100

    started work on main menu for snake game

--- ApiPlayground/ApiPlayground/obj/Release/net6.0/PubTmp/Out/wwwroot/js/SnakeGame.js
+++ ApiPlayground/ApiPlayground/obj/Release/net6.0/PubTmp/Out/wwwroot/js/SnakeGame.js
@@ -1,7 +1,21 @@
 ﻿var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
 
 window.onload = function load() {
-    canvas.style.width = "340px";
-    canvas.style.height = "500px";
+    //canvas.style.width = "340px";
+    //canvas.style.height = "500px";
+
+    canvas.width = 340;
+    canvas.height = 500;
+
+    canvas.style.backgroundColor = "black";
+    mainMenu();
 }
 
+
+function mainMenu(){
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "grey";
+    ctx.textAlign = "center";
+    ctx.fillText("Snake Game", 170, 70);
+}
